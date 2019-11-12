48955492df9322e6a72063f2e1990e01c96f4a5f
Patrick Niezen <patrickniezen@gmail.com>
2019-11-12 16:58:28 +0100

    Improved performance of Modal component.

--- burger-builder/src/components/UI/Modal/Modal.js
+++ burger-builder/src/components/UI/Modal/Modal.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Modal.module.css';
 import Aux from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed}></Backdrop>
-        <div className={classes.Modal} style={{
-            transform: props.show ? 'translateY(0)' : 'transformY(-100vh)',
-            display: props.show ? 'block' : 'none'
-        }}>
-            {props.children}
-        </div>
-    </Aux>
-)
+class Modal extends Component {
 
-export default modal;
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}></Backdrop>
+                <div className={classes.Modal} style={{
+                    transform: this.props.show ? 'translateY(0)' : 'transformY(-100vh)',
+                    display: this.props.show ? 'block' : 'none'
+                }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
+
+}
+
+export default Modal;
