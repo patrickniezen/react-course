88cb71e44d6ded25b633a8999c2fdebb1920341b
Patrick Niezen <patrickniezen@gmail.com>
2020-04-11 10:37:01 +0200

    Implemented outputting orders.

--- burger-builder/src/containers/Orders/Orders.js
+++ burger-builder/src/containers/Orders/Orders.js
@@ -1,17 +1,49 @@
 import React, { Component } from 'react';
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-order';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
-    state = {}
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(res => {
+                const fetchedOrders = []
+                for(let key in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+                this.setState({
+                    orders: fetchedOrders,
+                    loading: false
+                });
+            })
+            .catch(err => {
+                this.setState({
+                    loading: false
+                });
+            });
+    }
+
     render() {
         return (
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => (
+                    <Order 
+                        key={order.key}
+                        ingredients={order.ingredients}
+                        price={order.price} />
+                ))}
             </div>
         );
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
